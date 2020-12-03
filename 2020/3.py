755165d526ce2c4996a0ec4bad316c8a7809ad3f
Andrew Konoff <andrew.konoff@gmail.com>
2020-12-03 13:57:08 -0500

    3.2 solved

--- 2020/3.py
+++ 2020/3.py
@@ -14,14 +14,23 @@ def navigate(m, right, down):
     while current_row < (len(m) - 1):
         if m[current_row][current_position % 31] == "#":
             trees_hit += 1
-        current_row += 1
-        current_position += 3
+        current_row += down
+        current_position += right
 
+    print(trees_hit)
     return (trees_hit, current_position, current_row)
 
 with open('3.txt', 'r') as reader:
     the_map = reader.read()
     rows = the_map.split('\n')
 
-    info = navigate(rows, 3, 1)
-    print("You hit %s and navigated %s positions and %s rows" % (info[0], info[1], info[2]))
+    total_trees = 0
+
+    info_1 = navigate(rows, 1, 1)
+    info_2 = navigate(rows, 3, 1)
+    info_3 = navigate(rows, 5, 1)
+    info_4 = navigate(rows, 7, 1)
+    info_5 = navigate(rows, 1, 2)
+
+    total_trees = info_1[0] * info_2[0] * info_3[0] * info_4[0] * info_5[0]
+    print(total_trees)
