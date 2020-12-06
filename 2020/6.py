7cb5c9b97c0b67fdb842877a9e168a7a9fe6941a
Andrew Konoff <andrew.konoff@gmail.com>
2020-12-06 16:18:32 -0500

    6.2 solved

--- 2020/6.py
+++ 2020/6.py
@@ -4,10 +4,8 @@
 # The chords I played yesterday sound great today.
 # What a beautiful day. What a beautiful day.
 
-# add up the unique characters in each group
-# sum the count of unique characters for all groups
-
-
+# add up the characters that are in every person's answers in a given group
+# sum that for all groups
 
 with open('6.txt', 'r') as reader:
     lines = reader.read()
@@ -16,8 +14,28 @@ with open('6.txt', 'r') as reader:
     answer_count = 0
 
     for group in groups:
-        group = group.replace('\n','')
         group = group.strip('\n')
-        answer_count += len(set(group))
+        persons = group.split('\n')
+
+        all_characters = group.replace('\n','')
+        all_characters = all_characters.strip('\n')
+
+        answered_set = set(all_characters)
+
+        # now let's go through each person
+        answers = dict((el, 0) for el in answered_set)
+
+        for person in persons:
+            for answer in answered_set:
+                if answer in person:
+                    answers[answer] += 1
+
+        print(answers)
+        print(persons)
+        # now add whichever everyone had to the answer count
+        for answer, value in answers.items():
+            if value == len(persons):
+                print("adding 1 from %s for %s" % (answer, persons))
+                answer_count += 1
 
     print("There are %s unique answers" % answer_count)
